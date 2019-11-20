193bcc6fe38e7aaa4e744537be19eacb3832ee38
ReiCampo <54861362+ReiCampo@users.noreply.github.com>
2019-11-20 10:24:28 -0500

    Add files via upload

--- Programs:Coding/ContinuumSubtraction2.py
+++ Programs:Coding/ContinuumSubtraction2.py
@@ -31,17 +31,17 @@ lam_fit = np.concatenate([lam[blue_flux], lam[red_flux]])
 var_fit = np.concatenate([var2[blue_flux], var2[red_flux]])
 line_fit = np.poly1d(np.polyfit(lam_fit, flux_fit, deg = 1, w = var_fit))
 
-subtraction = flux - line_fit[flux]
+subtraction = flux - line_fit(lam)
 print(subtraction)
 
-#plt.plot(lam, flux, 'b')
+plt.plot(lam, subtraction, 'b')
 #plt.plot(bflux, flux[blue_flux], 'r')
 #plt.plot(rflux, flux[red_flux], 'r')
-#line = np.linspace(1400, 1800, 100)
-#plt.plot(line, line_fit(line), 'k')
-#plt.xlim(1400, 1800)
-#plt.xlabel('Wavelength (Angstroms)', fontsize = 10)
-#plt.ylabel('Flux', fontsize = 10)
-#plt.title('Spectra 7572-56944-0042', fontsize = 10)
+line = np.linspace(1400, 1800, 100)
+plt.plot(line, line_fit(line), 'k')
+plt.xlim(1400, 1800)
+plt.xlabel('Wavelength (Angstroms)', fontsize = 10)
+plt.ylabel('Flux', fontsize = 10)
+plt.title('Spectra 7572-56944-0042', fontsize = 10)
 
 
