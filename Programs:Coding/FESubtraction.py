1472db201c67f35ba46571b88f42bedc714cad6d
Ray Sharma <ramon.sharma1@gmail.com>
2020-02-06 08:24:38 -0500

    Adjusted Fe convolution, added linear rebinning

--- Programs:Coding/FESubtraction.py
+++ Programs:Coding/FESubtraction.py
@@ -15,10 +15,10 @@ import pandas as pd
 from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 from scipy.optimize import curve_fit
 
-platelist = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/platelist.fits')
-specdatalist = fits.open('/Users/RachelCampo/Desktop/Research/CIV-Variability/Programs/DR14Q_v4_4.fits')
-test_data = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/spec-7572-56944-0012.fits')
-FE_Template = pd.read_csv('/Users/RachelCampo/Desktop/Research/CIV-Variability/CIV-Variability-master/Fe_UVtemplt_A.dat', delim_whitespace = True)
+platelist = fits.open('/home/ray/Downloads/platelist.fits')
+specdatalist = fits.open('/home/ray/Downloads/DR14Q_v4_4.fits')
+test_data = fits.open('/home/ray/Downloads/spec-7572-56944-0012.fits')
+FE_Template = pd.read_csv('../Fe_UVtemplt_A.dat', delim_whitespace = True)
 
 # for the iron template, from the Vestergaard paper, the units are the same
 # as in the SDSS
@@ -49,26 +49,29 @@ def rebin_log(x, y):
 
 log_FE_wavelength, log_FE_spline = rebin_log(FE_wavelength, FE_flux)
 
-def fit_func(wavelength_rf, A, k, B, mu, sigma):
-    FE_convolution = np.convolve(log_FE_spline(wavelength_rf), gauss(wavelength_rf, mu, sigma), mode = 'same')
-    return (A * wavelength_rf**k) + (10**B * FE_convolution)
+def fitting_function(lam, A, k, B, mu, sigma):
+    global log_wavelength
+    global cutoffs
+    iron_convolution = np.convolve(log_FE_spline(log_wavelength), gauss(log_wavelength, mu, sigma), mode='same')
+    return (A * lam**k) + (10**B * iron_convolution[cutoffs])
+
 #the fit_func is fitting both the continuum AND the FE plate! That's why we are adding
     #both the Alambda^K and the FE.
 
 cutoffs = ((wavelength_rf > 1435)&(wavelength_rf < 1465)) | ((wavelength_rf > 1690)&(wavelength_rf < 1710))
 boundaries = [[0, -10, 0, 1000, 10], [100, 10, 20, 2000, 10000]]
 p0 = [10, -2, 13, 1000, 1000]
-log_wavelength, log_flux = rebin_log(wavelength_rf[cutoffs], flux_rf[cutoffs])
-pf, covariances = curve_fit(fit_func, log_wavelength, log_flux(log_wavelength), sigma = sigma[cutoffs], bounds = boundaries, p0 = p0)
+log_wavelength, log_flux = rebin_log(wavelength_rf, flux_rf)
+pf, covariances = curve_fit(fitting_function, log_wavelength[cutoffs], log_flux(log_wavelength[cutoffs]), sigma = sigma[cutoffs], bounds = boundaries, p0 = p0)
 
 
-C4_cutoffs = (log_wavelength > 1465) & (log_wavelength < 1710) # did not use the pf variables, possible reason
+cutoffs = (log_wavelength > 1465) & (log_wavelength < 1710) # did not use the pf variables, possible reason
 # why it was not subtracted off correctly.
-continuum_flux = fit_func(log_wavelength[C4_cutoffs], *pf) #put *pf in for the numbers
+continuum_flux = fitting_function(log_wavelength[cutoffs], *pf) #put *pf in for the numbers
 
-plt.plot(wavelength_rf[C4_cutoffs], flux_rf[C4_cutoffs], label = 'Original')
-plt.plot(wavelength_rf[C4_cutoffs], continuum_flux, label = 'Continuum + Iron')
-plt.plot(wavelength_rf[C4_cutoffs], flux_rf[C4_cutoffs] - continuum_flux, label = 'Subtracted Continuum')
+plt.plot(wavelength_rf[cutoffs], flux_rf[cutoffs], label = 'Original')
+plt.plot(wavelength_rf[cutoffs], continuum_flux, label = 'Continuum + Iron')
+plt.plot(wavelength_rf[cutoffs], flux_rf[cutoffs] - continuum_flux, label = 'Subtracted Continuum')
 plt.legend(loc = 'best')
 
 
