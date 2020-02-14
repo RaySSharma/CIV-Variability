07145d0245caadf2fd6076445f4dd22125d053f4
ReiCampo <54861362+ReiCampo@users.noreply.github.com>
2020-02-14 07:19:10 -0500

    Add files via upload

--- Programs:Coding/FESubtraction.py
+++ Programs:Coding/FESubtraction.py
@@ -15,6 +15,9 @@ import pandas as pd
 from scipy.interpolate import InterpolatedUnivariateSpline, interp1d
 from scipy.optimize import curve_fit
 
+# first I will import from the SDSS the platelist file, which containts all the
+# plates used on the telescope.
+# then I will import 
 platelist = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/platelist.fits')
 specdatalist = fits.open('/Users/RachelCampo/Desktop/Research/CIV-Variability/Programs/DR14Q_v4_4.fits')
 test_data = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/spec-7572-56944-0012.fits')
@@ -29,7 +32,6 @@ redshift = test_data[2].data['Z']
 flux_rf = test_data[1].data['flux'] / (1 + redshift) #no need to worry about rf the flux
 wavelength_rf = (10**wavelength) / (1 + redshift)
 ivar = test_data[1].data['ivar']
-sigma = 1 / np.sqrt(abs(ivar))
 
 #properties from iron template
 FE_wavelength = FE_Template['wavelength'].values
@@ -40,9 +42,11 @@ C4_bounds = (wavelength_rf > 1435) & (wavelength_rf < 1710)
 C4_flux = flux_rf[C4_bounds]
 C4_wavelength = wavelength_rf[C4_bounds]
 C4_ivar = ivar[C4_bounds]
+sigma = 1 / np.sqrt(abs(C4_ivar))
 
 
-#the three parameters for the widening of the iron plate
+#the three parameters for the widening of the iron plate, rebinning, and fitting
+#the function
 def gauss(x, m, sig):
     sigma_conv = np.sqrt(sig**2 - 900**2) / (2 * np.sqrt(2 * np.log(2)))
     broadened_sigma = np.exp(- (x - m)**2 / (2 * (sigma_conv)**2))
@@ -67,29 +71,41 @@ def fit_func(lam, A, k, B, m, sigma):
 #the fit_func is fitting both the continuum AND the FE plate! That's why we are adding
     #both the Alambda^K and the FE.
 
-boundaries = [[0, -10, 0, 1500, 10], [100, 10, 20, 1600, 10000]]
-p0 = [10, 0, 13, 1500, 1000]
+boundaries = [[0, -2, 10, 1500, 0], [100, 2, 20, 1600, 10000]]
+p0 = [10, 0, 14, 1550, 1000]
 log_wavelength, log_flux = rebin_log(C4_wavelength, C4_flux)
-cutoffs = ((log_wavelength > 1435)&(log_wavelength < 1465)) | ((log_wavelength > 1690)&(log_wavelength < 1710))
-pf, covariances = curve_fit(fit_func, log_wavelength[cutoffs], log_flux(log_wavelength[cutoffs]), sigma = sigma[cutoffs], bounds = boundaries, p0 = p0)
+ix = ((log_wavelength > 1435)&(log_wavelength < 1465)) | ((log_wavelength > 1690)&(log_wavelength < 1710))
+pf, covariances = curve_fit(fit_func, log_wavelength[ix], log_flux(log_wavelength[ix]), sigma = sigma[ix], bounds = boundaries, p0 = p0)
 
 
 C4_cutoffs = (log_wavelength > 1465) & (log_wavelength < 1710) # did not use the pf variables, possible reason
 # why it was not subtracted off correctly.
-continuum_flux = fit_func(log_wavelength[C4_cutoffs], *pf) #put *pf in for the numbers
+continuum_flux = fit_func(log_wavelength[ix], *pf) #put *pf in for the numbers
 
 #this will be in logspace
-plt.plot(log_wavelength[C4_cutoffs], log_flux[C4_cutoffs], label = 'Original')
-plt.plot(log_wavelength[C4_cutoffs], continuum_flux, label = 'Continuum + Iron')
-plt.plot(log_wavelength, log_flux(log_wavelength) - continuum_flux, label = 'Subtracted Continuum')
+plt.plot(log_wavelength[ix], log_flux(log_wavelength[ix]), label = 'Original')
+plt.plot(log_wavelength[ix], continuum_flux, label = 'Continuum + Iron')
+#plt.plot(log_wavelength, log_flux(log_wavelength) - continuum_flux, label = 'Subtracted Continuum')
+plt.legend(loc = 'best')
+
+plt.plot(log_wavelength, log_flux(log_wavelength), label = 'Original')
+plt.plot(log_wavelength, continuum_flux, label = 'Continuum + Iron')
+plt.plot(log_wavelength, log_flux(log_wavelength) - continuum_flux, label = 'Subtracted')
+plt.xlabel('wavelength')
+plt.ylabel('flux')
 plt.legend(loc = 'best')
 
 #converting back to linear space
-subtracted_log_flux = log_flux(log_wavelength) - continuum_flux
-lin_wavelength, lin_flux = rebin_lin(log_wavelength, subtracted_log_flux)
+subt_log_flux = log_flux(log_wavelength) - continuum_flux
+linear_wavelength, linear_flux = rebin_lin(log_wavelength, subt_log_flux)
 
-plt.plot(log_wavelength, subtracted_log_flux, label = 'Subtracted continuum in log space' )
-plt.plot(lin_wavelength, lin_flux(log_wavelength), label = 'Subtracted continuum in linear space')
+plt.plot(log_wavelength, log_flux(log_wavelength) - continuum_flux, label = 'Subtracted Continuum in log space')
+plt.plot(linear_wavelength, linear_flux(log_wavelength), label = 'Subtracted Continuum in lin space')
+plt.xlabel('flux')
+plt.ylabel('wavelength')
 plt.legend(loc = 'best')
+
+
+
 
 
