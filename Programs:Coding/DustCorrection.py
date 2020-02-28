a1b101d08b54c95438260cfc4a20c61e35aac827
ReiCampo <54861362+ReiCampo@users.noreply.github.com>
2020-02-28 10:27:54 -0500

    Add files via upload

--- Programs:Coding/DustCorrection.py
+++ Programs:Coding/DustCorrection.py
@@ -15,7 +15,7 @@ from dust_extinction.parameter_averages import F99
 
 
 platelist = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/platelist.fits')
-test_quasar = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/spec-7572-56944-0007.fits')
+test_quasar = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/spec-0266-51630-0053.fits')
 
 
 #Plate_RA = platelist[1].data['RACEN']
@@ -23,11 +23,13 @@ test_quasar = fits.open('/Users/RachelCampo/Desktop/Research/Data/Other Spectra/
 #plate = platelist[1].data['PLATE']
 RA = test_quasar[2].data['PLUG_RA']
 Dec = test_quasar[2].data['PLUG_DEC']
-wavelength = test_quasar[1].data['loglam']
+wavelength = 10**test_quasar[1].data['loglam']
 flux = test_quasar[1].data['flux']
 
+#plt.plot(wavelength, flux, 'b')
 
 coordinates = SC(RA, Dec, frame = 'icrs', unit = u.deg)
+#print(coordinates)
 
 sfd = SFDQuery()
 ebv = sfd(coordinates)
@@ -36,9 +38,10 @@ R_v = 3.1
 #print('EVB for test quasar:'.format(ebv))
 
 ext = F99(R_v)
+print(ext.extinguish(wavelength*u.AA, ebv))
 
 plt.semilogy(wavelength, flux, 'b', label = "Test Quasar's Emission")
-plt.semilogy(wavelength, flux/ext.extinguish(wavelength, ebv), 'r', label = 'Dereddened Quasar Emission')
+plt.semilogy(wavelength, flux/ext.extinguish(wavelength*u.AA, Ebv = ebv), 'y', label = 'Dereddened Quasar Emission')
 plt.xlabel('Wavelength (Angstrom)')
 plt.ylabel('Flux (ergs/cm^2/s)')
 plt.title('Dereddened Emission')
