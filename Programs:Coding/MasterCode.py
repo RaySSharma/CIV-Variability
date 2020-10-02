ffdc8001a40e3f5ddfe82aa07ae1ca56586377f6
ReiCampo <54861362+ReiCampo@users.noreply.github.com>
2020-10-02 08:36:45 -0400

    Update MasterCode.py

--- Programs:Coding/MasterCode.py
+++ Programs:Coding/MasterCode.py
@@ -17,7 +17,7 @@ import pandas as pd
 import tqdm
 
 spectra_dir = '/data2/rlc186/QuasarData/'
-spectra = ['spec-5202-55824-0105', 'spec-5202-55824-0106', 'spec-5202-55824-0112', 'spec-5202-55824-0115', 'spec-5202-55824-0118', 'spec-5202-55824-0152', 'spec-5202-55824-0180', 'spec-5202-55824-0194', 'spec-5202-55824-0224', 'spec-5202-55824-0295', 'spec-5202-55824-0308', 'spec-5202-55824-0338', 'spec-5202-55824-0340', 'spec-7128-56567-0120', 'spec-7128-56567-0171', 'spec-7128-56567-0178', 'spec-7128-56567-0270']
+spectra = glob.glob('/data2/rlc186/QuasarData/spec-*')
 final_list = []
 
 hdr = ['Name', 'MJD', 'Fiber ID', 'Plate', 'Redshift',
